7b6c26fe66e54933b336418df9efb925be79f5ef
Alfonso Socarras <asocarras10@gmail.com>
2019-09-11 19:30:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ def batch_badge_creator(names)
   message = []
   
   names.each do |name|
-    messages << "Hello, my name is #{name}."
+    message << "Hello, my name is #{name}."
   end
   
   message
